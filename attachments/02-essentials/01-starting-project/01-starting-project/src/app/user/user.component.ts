04b87c78bbbdb05fd6c9cfa5adb6ac07b1de87fd
VAL <nomeail@email.com>
2025-10-15 17:25:29 -0600

    Added input decorators

--- attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, computed } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import {DUMMY_USERS} from '../header/dummy-users';
 
@@ -12,17 +12,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string ;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string ;
+  @Input({required: true}) name!: string;
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar;
-  // }
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  });
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser(){
   }
