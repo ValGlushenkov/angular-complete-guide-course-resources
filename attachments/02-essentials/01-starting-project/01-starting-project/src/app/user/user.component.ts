c2e76f61b32aff1a5524d8cbfba05f13ee73aab8
VAL <nomeail@email.com>
2025-11-13 10:06:10 -0700

    Added tasks component

--- attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 import {DUMMY_USERS} from '../header/dummy-users';
 
@@ -15,7 +15,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string ;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
