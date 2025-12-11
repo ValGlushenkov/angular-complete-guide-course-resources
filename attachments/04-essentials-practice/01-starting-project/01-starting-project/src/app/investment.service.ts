856d8acd9c1540268c310963dc2226de3f75a829
VAL <nomeail@email.com>
2025-12-10 17:30:44 -0700

    Added code for header and user input components

---
+++ attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/investment.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from "@angular/core";
+import { type InvestmentInput } from "./investment-input.model";
+import { type AnnualData } from "./annual-data.model";
+
+@Injectable({providedIn:'root'})
+export class InvestmentService{
+    private investmentArr: InvestmentInput[] = [];
+    private annualData: AnnualData[] = [];
+
+    public addToInvestmentArr(
+        input: InvestmentInput
+    ){
+        this.investmentArr.push(input);
+    }
+
+    public calculateInvestmentResults(data: InvestmentInput) {
+    // const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+        const year = i + 1;
+        const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+        investmentValue += interestEarnedInYear + data.annualInvestment;
+        const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+        this.annualData.push({
+            year: year,
+            interest: interestEarnedInYear,
+            valueEndOfYear: investmentValue,
+            annualInvestment: data.annualInvestment,
+            totalInterest: totalInterest,
+            totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+        });
+    }
+
+    // return annualData;
+    }
+
+}
--- attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
+++ attachments/04-essentials-practice/01-starting-project/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+
+  initialInvestment:number = 0;
+  exepctedReturn:number = 0;
+  annualInvestment:number = 0;
+  duration:number = 0;
+
+  onSubmit(){
+    console.log('Form submitted');
+  }
 }
