39d3f2f0de763071659e8e210332bd29d56081c5
VAL <nomeail@email.com>
2025-12-22 09:39:26 -0700

    Added routing and config files

--- attachments/14-routing/starting-project/01-starting-project/src/main.ts
+++ attachments/14-routing/starting-project/01-starting-project/src/main.ts
@@ -1,14 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
-import { TasksComponent } from './app/tasks/tasks.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-    providers: [provideRouter([
-        {
-            path: 'tasks', // <your-domain/tasks>
-            component: TasksComponent
-        }
-    ])]
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
