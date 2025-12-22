02c0caf9930f48043ff97409f648a81e2b045d6d
VAL <nomeail@email.com>
2025-12-22 09:11:31 -0700

    Unzipped 14 folder and added some routes

---
+++ attachments/14-routing/starting-project/01-starting-project/src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { TasksComponent } from './app/tasks/tasks.component';
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter([
+        {
+            path: 'tasks', // <your-domain/tasks>
+            component: TasksComponent
+        }
+    ])]
+}).catch((err) => console.error(err));
