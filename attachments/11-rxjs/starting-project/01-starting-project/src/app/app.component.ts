ddb0917beee35ba0d5d4ae0c03416ca8c79f1507
VAL <nomeail@email.com>
2026-01-16 11:06:40 -0700

    Added observables and signals

--- attachments/11-rxjs/starting-project/01-starting-project/src/app/app.component.ts
+++ attachments/11-rxjs/starting-project/01-starting-project/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, computed, effect, inject, signal } from '@angular/core';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  clickCount = signal(0)
+  // interval = signal(0);
+  // dooubleInterval = computed(() => this.interval() * 2);
+  private destroyRef = inject(DestroyRef)
 
+  constructor(){
+    effect(() => {
+      console.log(`Clicked button ${this.clickCount()} times.`);
+    });
+  }
+ 
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.interval.update(prevIntervalNumber => prevIntervalNumber + 1);
+    // }, 1000);
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 2)
+    // ).subscribe({
+    //   next: (val) => console.log(val)
+    // });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe()
+    // });
+  }
+
+  onClick(){
+    this.clickCount.update(prevCount => prevCount + 1);
+  }
 }
