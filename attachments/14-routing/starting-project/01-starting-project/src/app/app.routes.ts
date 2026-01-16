b365a0e1a8724140a2b8cc4483eec9e17194ea65
VAL <nomeail@email.com>
2026-01-16 09:11:54 -0700

    Added router reload

--- attachments/14-routing/starting-project/01-starting-project/src/app/app.routes.ts
+++ attachments/14-routing/starting-project/01-starting-project/src/app/app.routes.ts
@@ -11,7 +11,7 @@ const dummyCanMatch: CanMatchFn = (
 ) => {
     const router = inject(Router);
     const shouldGetAccess = Math.random();
-    if(shouldGetAccess < 0.5){
+    if(shouldGetAccess < 1){
         return true;
     }
     return new RedirectCommand(router.parseUrl('/unauthorized'));
