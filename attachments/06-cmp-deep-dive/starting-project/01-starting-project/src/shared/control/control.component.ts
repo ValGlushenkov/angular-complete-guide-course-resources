09a0fcde61de7bd07b73a273801500e89dd8cf06
ValGlushenkov <glushval@gmail.com>
2026-01-30 20:38:27 -0700

    Added Hosr listener

--- attachments/06-cmp-deep-dive/starting-project/01-starting-project/src/shared/control/control.component.ts
+++ attachments/06-cmp-deep-dive/starting-project/01-starting-project/src/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,17 @@ import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core'
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    // '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
   // @HostBinding('class') className = 'control';
+  @HostListener('click') onClick(){
+    console.log('Clicked!');
+
+  }
   label = input.required<string>();
+
+
 }
